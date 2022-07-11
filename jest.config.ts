c823f40eb92e37ff3f749e0b35ed6b6c891594b5
Matheus Leal <matheusspleal@gmail.com>
2022-07-11 16:38:40 -0300

    chore: add jest and ts-jest

---
+++ jest.config.ts
@@ -0,0 +1,19 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { pathsToModuleNameMapper } from 'ts-jest'
+
+import { compilerOptions } from './tsconfig.json'
+
+export default {
+  bail: true,
+  clearMocks: true,
+  collectCoverage: true,
+  collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
+  coverageDirectory: 'coverage',
+  coverageProvider: 'v8',
+  coverageReporters: ['text-summary', 'lcov'],
+  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, {
+    prefix: '<rootDir>/src/',
+  }),
+  preset: 'ts-jest',
+  testMatch: ['**/*.spec.ts', '**/*.test.ts'],
+}
